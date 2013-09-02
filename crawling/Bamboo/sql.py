500d2ea874d9c4970ff9099a17007b8b2e4969d1
thibault <thibault@thibault-VirtualBox.(none)>
2013-09-02 17:35:44 +0100

    Insert descriptions tests

--- crawling/Bamboo/sql.py
+++ crawling/Bamboo/sql.py
@@ -13,6 +13,7 @@ def getObjects(path):
 	BS = BeautifulSoup(R)
 	id = 0
 	dic = {}
+	rev = {}
 	ids = {}
 	for E in BS.find_all("element"):
 		id += 1 
@@ -21,28 +22,31 @@ def getObjects(path):
 		
 			if T.name in dic:
 				if T.name not in ids[id]:
-					ids[id][T.name] = set()
+					ids[id][T.name] = list()
 
 				if T.string in dic[T.name]:
 					#print dic[T.name][T.string]
-					ids[id][T.name].add(dic[T.name][T.string])
+					ids[id][T.name].append(dic[T.name][T.string])
 					
 				else:
 					#print dic[T.name][T.string]
-					dic[T.name][T.string] = len(dic[T.name]) + 1
-					ids[id][T.name].add(dic[T.name][T.string])
+					i = len(dic[T.name]) + 1
+					dic[T.name][T.string] = i
+					rev[T.name][i] = T.string
+					ids[id][T.name].append(dic[T.name][T.string])
 					
 			else:
 			
 				dic[T.name] = { T.string : 1}
-				ids[id][T.name] = set()
-				ids[id][T.name].add(1)
+				rev[T.name] = { 1 : T.string }
+				ids[id][T.name] = list()
+				ids[id][T.name].append(1)
 				
 
 	#print dic
-	return dic, ids
+	return dic, ids, rev
 	
-dic, ids = getObjects("./output/bamboodirt.xml")
+dic, ids, rev = getObjects("./output/bamboodirt.xml")
 
 #This synonym Table is used for external/relative entries such as keywords or platform (Tool > has_XYZ > XIZ)
 tableNID = {"tags" : "Keyword", "platform" : "Platform", "categories" : "Tool_type"} # Without int UID stuff
@@ -53,11 +57,54 @@ tableWID = {"developer" : "Developer"} #With int UID Stuff
 for T in dic:
 	if T in tableNID:
 		for E in dic[T]:
-			print "INSERT INTO " + tableNID[T] + " VALUES ('" + E + "')"
+			print "INSERT INTO " + tableNID[T] + " VALUES ('" + E + "');"
 	elif T in tableWID:
 		for E in dic[T]:
 			#print dic[T]
-			print "INSERT INTO " + tableWID[T] + " VALUES ('" + str(dic[T][E]) + "', '" + E + "')"
+			print "INSERT INTO " + tableWID[T] + " VALUES ('" + str(dic[T][E]) + "', '" + E + "');"
 	else:
 		pass
 		#print T;
+
+for E in ids:
+	print "INSERT INTO Tool VALUES ("+str(E)+", '"+ rev["name"][ids[E]["name"][0]] +"')"
+	s = "INSERT INTO Description VALUES ('', '"+ rev["name"][ids[E]["name"][0]] +"', "
+	
+	if "description" in ids[E] and ids[E]["description"][0] in rev["description"]:
+		print ids[E]["description"][0]
+	
+		s += " '"+ str(rev["description"][ids[E]["description"][0]]) +"', "
+	else:
+		s += " '', "
+	
+	#No version in BambooDirt
+	s += " '', "
+	
+	
+	if "webpage" in ids[E]:
+		s += " '"+ rev["webpage"][ids[E]["webpage"][0]] +"', "
+	else:
+		s += " '', "
+		
+	#No availableFrom in BambooDirt
+	s += " '', "
+	s += " 'DATE', 'USER_UID', 'author',  "
+	
+	if "license" in ids[E]:
+		s += " '"+ rev["license"][ids[E]["license"][0]] +"', "
+	else:
+		s += " '', "
+		
+	if "categories" in ids[E]:
+		s += " '"+ rev["categories"][ids[E]["categories"][0]] +"', "
+	else:
+		s += " '', "
+		
+	#No Application_type in BambooDirt
+	s += " '', "
+	
+	#TOOL UID +  USERS_UID
+	print E
+	s += str(E) + ", 'USER_UID' "
+		
+	print s+");"
