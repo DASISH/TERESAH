65129734608bb0b4235b6b7fcba843776c7f6a7b
Olof Olsson <borsna@gmail.com>
2013-09-16 16:57:49 +0200

    reformat routes

--- www/RDF/routes.php
+++ www/RDF/routes.php
@@ -1,9 +1,9 @@
 <?php
-    $app->get('/dump.rdf', function () use ($rdf, $app) { 
-        $app->response->headers->set('Content-Type', 'text/xml');
-        print $app->render('rdf.php', array('data' => $rdf->all()));
-    });
-    $app->get('/dump.ttl', function () use ($rdf, $app) {
-        render_ttl($rdf->all());
-    });      
+$app->get('/dump.rdf', function () use ($rdf, $app) { 
+    $app->response->headers->set('Content-Type', 'text/xml');
+    print $app->render('rdf.php', array('data' => $rdf->all()));
+});
+$app->get('/dump.ttl', function () use ($rdf, $app) {
+    render_ttl($rdf->all());
+});      
 ?>
