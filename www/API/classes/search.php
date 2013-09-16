8cac84075f9b6ab7b17338662047e4169c140d55
Thibault Clerice <leponteineptique@gmail.com>
2013-09-16 13:51:34 +0100

    API : Search in facets

--- www/API/classes/search.php
+++ www/API/classes/search.php
@@ -5,6 +5,45 @@
 			global $DB;
 			$this->DB = $DB;
 		}
+		function options($get, $queryNeeded = False) {
+			$options = array();
+			
+			#Query
+			if(!isset($get["request"])) {
+				if($queryNeeded) {
+					return array("error" => "No request given"); 
+				} else {
+					$options["request"] = Null;
+				}
+			} else { 
+				$options["request"] = $get["request"];
+			}
+			
+			#CASE
+			if(!isset($get["case_insensitivity"])) { 
+				$options["case_insensitivity"] = false; 
+				$sensitivity = ""; 
+			} else {
+				$options["case_insensitivity"] = true;
+				$sensitivity = "COLLATE utf8_general_ci"; 
+			}
+			
+			#LIMIT
+			if(!isset($get["limit"]) || intval($get["limit"]) > 50) { 
+				$options["limit"] = 20; 
+			} else { 
+				$options["limit"] = (int) $get["limit"]; 
+			}
+			
+			#START
+			if(!isset($get["start"])) {
+				$options["start"] = 0; 
+			} else { 
+				$options["start"] = (int) $get["start"]; 
+			}
+			
+			return array($options, $sensitivity);
+		}
 		function general($get) {
 			#####
 			#
@@ -18,11 +57,9 @@
 			#
 			#####
 			//Default research search in keyword, description AND external Description AND Application_Type
-			$options = array();
-			if(!isset($get["request"])) { return array("error" => "No request given"); } else { $options["request"] = $get["request"];}
-			if(!isset($get["case_insensitivity"])) { $options["case_insensitivity"] = false; $sensitivity = ""; } else {$options["case_insensitivity"] = true; $sensitivity = "COLLATE utf8_general_ci"; }
-			if(!isset($get["limit"]) || intval($get["limit"]) > 50) { $options["limit"] = 20; } else { $options["limit"] = (int) $get["limit"]; }
-			if(!isset($get["start"])) { $options["start"] = 0; } else { $options["start"] = (int) $get["start"]; }
+			$options = $this->options($get, $queryNeeded = True);
+			$options = $opt[0];
+			$sensitivity = $opt[1];
 			#$reqWord = "xml";
 			// $reqWord = "%".$reqWord."%";
 			
@@ -57,6 +94,85 @@
 			
 			
 			return $ret;
+		}
+		function fieldContent($fieldType,$get) {
+			##########
+			#
+			#	fieldtype : 
+			#		*	Suite
+			#		*	Feature
+			#		*	Platform
+			#		*	Keyword
+			#		*	Project
+			#		*	Standard
+			#		*	Publication
+			#		*	Developer
+			#		*	ApplicationType
+			#		*	ToolType
+			#		*	Organization
+			#		*	LicenceType
+			#		*	Licence
+			#
+			##########
+			$dict = array(
+					#option value	=> Table Name, Text field (if many = array with [0] as title for it), PKey 
+					"Suite" => array("Suite", "name", "UID"),
+					"Feature" => array("Feature", array("name", "description"), "name"),
+					"Platform" => array("Platform", "platform", "platform"),
+					"Project" => array("Project", array("title", "description"), "UID"),
+					"Standard" => array("Standard", "title", "UID"),
+					"Keyword" => array("Keyword", "keyword", "keyword_uid"),
+					"Publication" => array("Publication", "reference", "UID"),
+					"Developer" => array("Developer", "name", "UID"),
+					"ApplicationType" => array("Application_type", "application_type", "application_type"),
+					"ToolType" => array("Tool_type", "tool_type", "tool_type"),
+					"Organization" => array("Organization", "name", "UID"),
+					"LicenceType" => array("Licence_type", "licence_type", "licence_type"),
+				);
+			if(isset($dict[$fieldType])) {
+				#Get Options
+				$opt = $this->options($get);
+				$options = $opt[0];
+				$sensitivity = $opt[1];
+				#Set special option
+				$options["field"] = $fieldType;
+				
+				$sql = $dict[$fieldType];
+				
+				#Setting SQL Request:
+				#Setting var we will use :
+				$where = "";
+				$exec = array();
+				
+				#If we got more than one field to search
+				if (is_array($sql[1])) {
+					$retField =  $sql[1][0];
+					if($options["request"] != Null) {
+						$where = array();
+						foreach($sql[1][0] as &$value) {
+							$where[] = " ".$value." LIKE CONCAT('%', ? , '%') ".$sensitivity. " ";
+							$exec[] = $options["request"];
+						}
+						$where = "WHERE "+implode($where, ",");
+					}
+					
+				} else {
+					$retField = $sql[1];
+					if($options["request"] != Null) {
+						$where = "WHERE ".$sql[1]." LIKE CONCAT('%', ? , '%') ".$sensitivity. " ";
+						$exec[] = $options["request"];
+					}
+				}
+				$ret = $retField." as name, ".$sql[2]." as id";
+				
+				$req = "SELECT " . $ret . " FROM " . $sql[0] . " " .$where ." LIMIT ".$options["start"]." , ".$options["limit"];
+				// return $req;
+				$req = $this->DB->prepare($req);
+				$req->execute($exec);
+				return $req->fetchAll(PDO::FETCH_ASSOC);
+			} else {
+				return array("Error" => "Field doesn't exist");
+			}
 		}
 	}
 	$search = new Search();
--- www/API/routes/search.php
+++ www/API/routes/search.php
@@ -3,4 +3,8 @@
 	$app->get('/search/general', function () use ($search, $app) { 
 		return jP($search->general($app->request->get())); 
 	});
+	
+	$app->get('/search/facet/:field', function ($facet) use ($search, $app) { 
+		return jP($search->fieldContent($facet, $app->request->get())); 
+	});
 ?>
