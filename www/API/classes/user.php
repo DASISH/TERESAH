8d636ef59d398022a0fd7511ec72fd1a164099a9
Clérice Thibault <leponteineptique@gmail.com>
2013-09-20 16:41:04 +0100

    Bugged PDO ???

--- www/API/classes/user.php
+++ www/API/classes/user.php
@@ -3,13 +3,22 @@
 		function __construct() {
 			#Gettings globals
 			global $DB;
+			if(!isset($DB)) { exit(); }
 			$this->DB = $DB;
 		}
 		function login($post) {
-			$req = "SELECT u.name, u.mail FROM User u WHERE u.Login = ? AND u.Password = ? LIMIT 1";
-			$req = $this->DB->prepare($req);
-			$req->execute(array($post->user, hash('sha256', $post->password)));
-			return array($req->fetch(PDO::FETCH_ASSOC), array($post->user, hash('sha256', $post->password)));
+			$pw = hash('sha256', $post->password);
+			
+			try {
+			$req = $this->DB->prepare("SELECT * FROM User WHERE Name = ? ");
+			$req->execute(array("Thibault"));     
+			} catch (Exception $e) {
+				Die('Need to handle this error. $e has all the details');
+			}
+			
+			//$req = $this->DB->prepare("SELECT u.name, u.mail FROM User u WHERE u.Login = 'titi' AND u.Password = '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5'");
+			// $req->execute();
+			return array($req->rowCount(),$req->fetch(PDO::FETCH_ASSOC), array($post->user, hash('sha256', $post->password)));
 			/*
 			if($req->rowCount() == 1) {
 				return array("signin" => true, "data" => $req->fetch(PDO::FETCH_ASSOC));
