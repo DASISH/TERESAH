271793e30e2ff73840b536952f4c1667bdb3ad61
Olof Olsson <borsna@gmail.com>
2013-10-15 16:59:12 +0200

    added icons

--- www/ADMIN/classes/statistics.php
+++ www/ADMIN/classes/statistics.php
@@ -6,7 +6,8 @@ class Statistics {
     }
     function all(){
         return array(
-            'tool' => $this->tools()
+            'tool' => $this->tools(),
+            'user' => $this->users()
         );
     }
     
@@ -22,6 +23,20 @@ class Statistics {
        
        return $result;
     }
+
+    function users(){
+       $result = array();
+       $query = "SELECT COUNT(user_uid) AS count FROM user";
+       $req = $this->DB->prepare($query);
+       $req->execute();
+       $users = $req->fetch(PDO::FETCH_ASSOC);
+       
+       $result['count'] = $users['count'];
+       
+       
+       return $result;
+    }    
+    
 }
 $statistics = new Statistics();
 ?>
--- www/ADMIN/templates/statistics.php
+++ www/ADMIN/templates/statistics.php
@@ -1,9 +1,32 @@
-
-<div class="panel panel-default">
-  <div class="panel-heading">Tools</div>
-  <div class="panel-body">
-   Count :<?php print $statistics['tool']['count'];?>
-  </div>
+<div class="col-xs-12 col-sm-9">
+    <div class="row">
+        <div class="col-6 col-sm-6 col-lg-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                  <span class="glyphicon glyphicon-briefcase"></span>
+                  Tools
+              </div>
+              <div class="panel-body">
+               Count :<?php print $statistics['tool']['count'];?><br />
+               <a href="#"><span class="glyphicon glyphicon-plus"></span>Add tool</a>
+              </div>
+            </div>            
+        </div>
+        <div class="col-6 col-sm-6 col-lg-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                  <span class="glyphicon glyphicon-user"></span>
+                  Users
+              </div>
+              <div class="panel-body">
+               Count :<?php print $statistics['user']['count'];?>
+              </div>
+            </div>            
+        </div>        
+    </div>
+    
 </div>
+
+
 
 
