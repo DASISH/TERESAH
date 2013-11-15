c6a49f5d70a33c9ed64adbd8d4629302b61ee818
Johan Fihn <johanfihn@gmail.com>
2013-11-15 12:15:08 +0100

    Removed git hub crap

--- www/ADMIN/templates/log_list.php
+++ www/ADMIN/templates/log_list.php
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<h1>Logs</h1>
-<!--
-<a href="<?php print BASE_PATH; ?>tool/add"><span class="glyphicon glyphicon-plus"></span> Add</a>  
-<a href="#"><span class="glyphicon glyphicon-check"></span> Select all</a>
-<a href="#" class="batch-action" data-batch-function="deleteTools"><span class="glyphicon glyphicon-trash"></span> Delete</a>
--->
-=======
-<?php print_r($logs); ?>
-
 <div class="page-header">
     <h1>Logs</h1>
     <div class="toolbox-fixed">
@@ -18,7 +8,6 @@
         --> 
     </div>
 </div>
->>>>>>> 3b9b8ae118bbc08da2bd686f3977473ee9a3b3ce
 
 <table class="sortable table table-striped table-bordered">
     <thead>
