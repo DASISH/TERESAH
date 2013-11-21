ed7a4a412293c0c83cdcd69b18a813c9df5517a9
Clérice Thibault <leponteineptique@gmail.com>
2013-11-21 16:05:37 +0000

    API Classes : documented code for Require

--- www/API/classes/requires.php
+++ www/API/classes/requires.php
@@ -1,53 +1,81 @@
 <?php
 	class requires {
+	/**
+	 * Handles dependencies of class to avoid inserting all classes  at the beginning
+	 *
+	 *
+	 */
+	 
+	/**
+	 *	Set the path to files to be included
+	 * @return void
+	 */
 		function __construct($path) {
 			$this->path = $path;
 		}
 		
+	/**
+	 *	Include a file	
+	 *
+	 * @param	$file	Call a file
+	 * @return void
+	 */
 		private function r($file) {
 			require_once($this->path."/".$file);
 		}
 		
+	/**
+	 *	Calls dependencies for a given class name
+	 *
+	 * @param	$dependency		Class identifier
+	 * @return void
+	 */
 		private function sw($dependency) {
 			switch($dependency) {
 				case "graphic":
 					$this->r("graphic.php");
-					break;
+				break;
 				case "description":
 					$this->r("descriptions.php");
-					break;
+				break;
 				case "search":
 					$this->r("helper.php");
 					$this->r("search.php");
-					break;
+				break;
 				case "helper":
 					$this->r("helper.php");
-					break;
+				break;
 				case "comment":
 					$this->r("comments.php");
-					break;
+				break;
 				case "tool":
 					$this->r("facets.php");
 					$this->r("tool.php");
 					$this->r("descriptions.php");
-					break;
+				break;
 				case "facet":
 					$this->r("helper.php");
 					$this->r("facets.php");
-					break;
+				break;
 				case "description":
 					$this->r("helper.php");
 					$this->r("descriptions.php");
-					break;
+				break;
 				case "user":
 					$this->r("user.php");
-					break;
+				break;
 				case "log":
 					$this->r("log.php");
-					break;
+				break;
 			}
 		}
 	
+	/**
+	 *	From a given var, call dependencies
+	 *
+	 * @param	$array		String or array of class identifier
+	 * @return void
+	 */
 		public function req($array) {
 			if(is_array($array)) {
 				foreach($array as $dependency) {
@@ -58,5 +86,9 @@
 			}
 		}
 	}
+	
+	/*
+	 *	Instantiate the require class
+	 */
 	$require = new requires(dirname(__FILE__));
 ?>
