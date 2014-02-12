a3dda6637ee6f7adf178a562c066f95d3bed615d
jfihn <johan.fihn@gmail.com>
2014-02-12 12:44:34 +0100

    Cleanup

--- www/ADMIN/routes/tool.php
+++ www/ADMIN/routes/tool.php
@@ -1,11 +1,11 @@
 <?php
 
 $app->get('/tool', function () use ($tool){ 
-    display('tool_list.php', array('tools' => $tool->listAll()));
+    display('tool_list.php', array('tools' => AdminTool::listAll()));
 });
 
 $app->get('/tool/:shortname', function ($shortname) use ($tool){ 
-    display('forms/tool_edit.php', array('tool' => $tool->getTool($shortname)));
+    display('forms/tool_edit.php', array('tool' => AdminTool::getTool($shortname)));
 });
 
 ?>
--- www/ADMIN/routes/user.php
+++ www/ADMIN/routes/user.php
@@ -8,23 +8,23 @@ $app->get('/user', function () {
 /* Edit user */
 $app->get('/user/edit/:user_uid', function ($user_uid) use ($app){ 
 
-	$form_address = $app->urlFor('user_edit_post', array('user_uid' => $user_uid));
+    $form_address = $app->urlFor('user_edit_post', array('user_uid' => $user_uid));
 
     display('forms/user_edit.php', array('user' => AdminUser::getUserByID($user_uid), 
-										 'form_address' => $form_address,
-										 'title' => 'Edit user'));
+                                        'form_address' => $form_address,
+                                        'title' => 'Edit user'));
 })->name('user_edit');
 
 /* Edit user post */
 $app->post('/user/edit/:user_uid', function ($user_uid) use ($app) {
     
 	$result = AdminUser::update($user_uid,
-								$app->request->post('name'),
-								$app->request->post('mail'),
-								$app->request->post('login'),
-								$app->request->post('password'),
-								$app->request->post('user_active') == 'on' ? 1 : 0,
-								$app->request->post('user_admin') == 'on' ? 1 : 0);
+                                    $app->request->post('name'),
+                                    $app->request->post('mail'),
+                                    $app->request->post('login'),
+                                    $app->request->post('password'),
+                                    $app->request->post('user_active') == 'on' ? 1 : 0,
+                                    $app->request->post('user_admin') == 'on' ? 1 : 0);
 	
 	if(isset($result['success'])) {
 		$app->flash('success', $result['success']);
@@ -42,19 +42,19 @@ $app->get('/user/add', function () use ($app){
 	$form_address = $app->urlFor('user_add_post');
 
 	display('forms/user_edit.php', array('user' => array(), 
-										 'form_address' => $form_address, 
-										 'title' => 'Add user'));
+                                            'form_address' => $form_address, 
+                                            'title' => 'Add user'));
 })->name('user_add');
 
 /* Add user post */
 $app->post('/user/add', function () use ($app) {
 
     $result = AdminUser::create($app->request->post('name'), 
-							  	$app->request->post('mail'), 
-								$app->request->post('login'), 
-								$app->request->post('password'),
-								$app->request->post('user_active') == 'on' ? 1 : 0,
-								$app->request->post('user_admin') == 'on' ? 1 : 0);
+                                $app->request->post('mail'), 
+                                $app->request->post('login'), 
+                                $app->request->post('password'),
+                                $app->request->post('user_active') == 'on' ? 1 : 0,
+                                $app->request->post('user_admin') == 'on' ? 1 : 0);
 	
 	if(isset($result['success'])) {
 		$app->flash('success', $result['success']);
