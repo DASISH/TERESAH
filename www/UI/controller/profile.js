c0ed66e58f67b894c98fa8c996320389618692c6
PonteIneptique <leponteineptique@gmail.com>
2014-03-06 09:22:29 +0000

    Update profile.js

--- www/UI/controller/profile.js
+++ www/UI/controller/profile.js
@@ -1,6 +1,14 @@
 var Profile = portal.controller('ProfileCtrl', ['$scope', 'ui', 'Item', '$rootScope', '$window', function($scope, $ui, $item, $root, $window) {
-
-        $ui.user.signedin();
+        $scope.ui = {
+                user : {
+                        data : {
+                                
+                        }
+                }
+        }
+        $ui.user.signedin(function (data) {
+                $scope.ui.user.data = data;
+        });
         $ui.title("Profile");
 
         //exec
