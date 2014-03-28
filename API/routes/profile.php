93bbd3350b8f94a5dd3fe38ecb8efb80406f36a7
Olof Olsson <borsna@gmail.com>
2014-03-28 15:44:57 +0100

    formating fix and password complexity check for profile

--- API/routes/profile.php
+++ API/routes/profile.php
@@ -1,33 +1,34 @@
 <?php
 
-$app->post('/profile/', function () use ($require, $app) {
+$app->post('/profile/', function () use ($require, $app){
 
-    if (!isset($_SESSION["user"]["id"])) {
+    if (!isset($_SESSION["user"]["id"])){
         return jP(array("status" => "error", "message" => "You need to be logged in to use this function"));
     }
 
-	$require->req("user");
-		
-	if(count($app->request->post()) > 0) {
-		$input = $app->request->post();
-	} elseif(count($app->request()->getBody()) > 0) {
-		$input = $app->request()->getBody();
-	} else {
-		$app->response()->status(400);
-	}
+    $require->req("user");
 
-	$post = array();
-	if(isset($input["name"])) {
-		$post["name"] = $input["name"];
-	}
-	if(isset($input["mail"])) {
-		$post["mail"] = $input["mail"];
-	}
-	if(isset($input["password"])) {
-		$post["password"] = $input["password"];
-	}
+    if (count($app->request->post()) > 0){
+        $input = $app->request->post();
+    }
+    elseif (count($app->request()->getBody()) > 0){
+        $input = $app->request()->getBody();
+    }
+    else{
+        $app->response()->status(400);
+    }
+
+    $post = array();
+    if (isset($input["name"])){
+        $post["name"] = $input["name"];
+    }
+    if (isset($input["mail"])){
+        $post["mail"] = $input["mail"];
+    }
+    if (isset($input["password"])){
+        $post["password"] = $input["password"];
+    }
 
     return jP(User::update($_SESSION["user"]["id"], $post));
 });
-
 ?>
