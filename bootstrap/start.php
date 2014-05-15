f9b2d28da256a94076253f78cc4006a63bb89336
Sami Granstedt <sami.granstedt@uta.fi>
2014-05-15 10:19:34 +0300

    Add "teresah" to the list of default development environments

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-  "development" => array("*.local", "*.localhost"), 
+  "development" => array("teresah", "*.local", "*.localhost"), 
 ));
 
 /*
