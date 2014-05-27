6732493f0489fe3eed9b27d18472e25923124424
Sami Granstedt <sami.granstedt@uta.fi>
2014-05-27 10:57:20 +0300

    Update the PagesController to follow the PSR-1

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -19,23 +19,23 @@ class PagesController extends BaseController
      * TODO: Review the security of matchStaticView() function. Does 
      * the Laravel framework already filter the "Request::path()" or 
      * "View::make()" methods, or do we need to filter out possible 
-     * directory traversal attacks from the "request_path" variable?
+     * directory traversal attacks from the "requestPath" variable?
      * 
      * @return Response
      */
     private function matchStaticView()
     {
-        $app_path = rtrim(app_path(), "/");
-        $request_path = rtrim(mb_strtolower(Request::path()), "/");
-        $full_static_view_path = "{$app_path}/views/pages/{$request_path}";
-        $static_view_filename = "pages/{$request_path}";
+        $appPath = rtrim(app_path(), "/");
+        $requestPath = rtrim(mb_strtolower(Request::path()), "/");
+        $fullStaticViewPath = "{$appPath}/views/pages/{$requestPath}";
+        $staticViewFilename = "pages/{$requestPath}";
 
-        if (is_dir($full_static_view_path)) {
-            $static_view_filename .= "/index";
+        if (is_dir($fullStaticViewPath)) {
+            $staticViewFilename .= "/index";
         }
 
-        if (View::exists($static_view_filename)) {
-            return View::make($static_view_filename);
+        if (View::exists($staticViewFilename)) {
+            return View::make($staticViewFilename);
         }
 
         # Otherwise return the 404 response
