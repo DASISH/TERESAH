1b3b35cd842e1d8e86ca71cdec01bce9edd2d3e3
Sami Granstedt <sami.granstedt@uta.fi>
2014-06-12 12:27:31 +0300

    Use CSRF protection filter on all POST, PUT, PATCH and DELETE

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,12 @@
 
 class BaseController extends Controller
 {
+    public function __construct()
+    {
+        $this->beforeFilter("csrf", array("on" => 
+            "post", "put", "patch", "delete"));
+    }
+
     /**
      * Setup the layout used by the controller.
      *
