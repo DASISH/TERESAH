cedb20fde5002ecb7e79464a7fffa7c5c46a1a83
Sami Granstedt <sami.granstedt@uta.fi>
2014-06-12 13:24:26 +0300

    Disable the search field until search is implemented

--- app/views/shared/_search.blade.php
+++ app/views/shared/_search.blade.php
@@ -1,9 +1,9 @@
 <form class="navbar-form navbar-right hidden-sm">
     <div class="form-group">
-        <input type="text" placeholder="{{ Lang::get("views/pages/navigation.search.placeholder") }}" name="quicksearch" class="form-control input-sm">
+        <input type="text" placeholder="{{ Lang::get("views/pages/navigation.search.placeholder") }}" name="quicksearch" class="form-control input-sm" disabled>
     </div>
     <!-- /form-group -->
 
-    <button type="submit" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-search"></span></button>
+    <button type="submit" class="btn btn-success btn-sm" disabled><span class="glyphicon glyphicon-search"></span></button>
 </form>
 <!-- /navbar-form.navbar-right.hidden-sm -->
