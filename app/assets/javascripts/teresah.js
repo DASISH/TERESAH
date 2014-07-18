8d1188b5fe3fc48ca8735275ec8723c9cc79d919
Sami Granstedt <sami.granstedt@uta.fi>
2014-07-18 10:23:36 +0300

    Add ability to DELETE through link requests

--- app/assets/javascripts/teresah.js
+++ app/assets/javascripts/teresah.js
@@ -2,6 +2,17 @@
 TERESAH JavaScript
 ---------------------------------------------------------------------------- */
 
+// Mimics the rails.js's handleMethod() function
+function handleMethod(link) {
+    var href = link.attr("href");
+    var method = link.data("method");
+    var form = $("<form action=\"" + href + "\" method=\"POST\"></form>");
+    var metaDataInput = "<input type=\"hidden\" name=\"_method\" value=\"" + method + "\">";
+
+    form.hide().append(metaDataInput).appendTo("body");
+    form.submit();
+}
+
 $(document).ready(function() {
     // Initialize the jQuery Autosize
     $("textarea").autosize();
@@ -11,6 +22,18 @@ $(document).ready(function() {
     $(".editable").editable({
         ajaxOptions: {
             type: "PUT"
+        }
+    });
+
+    // Add an event handler to listen DELETE through link requests
+    $("a[data-method=\"delete\"]").on("click", function(event) {
+        var link = $(this);
+        var message = link.data("confirm");
+
+        event.preventDefault();
+
+        if (!message || confirm(message)) {
+            handleMethod(link);
         }
     });
 });
