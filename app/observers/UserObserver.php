06b7f29bb64ead197464d58a4f9fdc7e8c541b09
Olof Olsson <borsna@gmail.com>
2014-09-11 11:16:08 +0200

    dont send email of app is running via CLI
    eg. php artisan db:seed

--- app/observers/UserObserver.php
+++ app/observers/UserObserver.php
@@ -4,15 +4,16 @@ class UserObserver
 {
     public function created($user)
     {
-        $locale = App::getLocale();
+        if (App::runningInConsole() === false) {
+            $locale = App::getLocale();
 
-        # TODO: Use queue when sending e-mail messages
-        Mail::send(array("text" => "mailers.signup.welcome_{$locale}"), 
-            array("user" => $user), function($message) use ($user) {
-            $message->to($user->email_address, $user->name);
-            $message->subject("[TERESAH] ".Lang::get("mailers/signup.welcome.subject"));
-        });
-
+            # TODO: Use queue when sending e-mail messages
+            Mail::send(array("text" => "mailers.signup.welcome_{$locale}"), 
+                array("user" => $user), function($message) use ($user) {
+                $message->to($user->email_address, $user->name);
+                $message->subject("[TERESAH] ".Lang::get("mailers/signup.welcome.subject"));
+            });
+        }
         # Ensure we always return true (in order not to 
         # break the callback/event chain), even if sending
         # of the welcome e-mail fails.
