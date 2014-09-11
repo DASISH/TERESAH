c0e23835cd0b2a0f9dbdb14040d418c6aa1744d9
Olof Olsson <borsna@gmail.com>
2014-09-11 16:05:06 +0200

    removed testusers from seed

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -16,39 +16,6 @@ class UsersTableSeeder extends Seeder
                 "created_at" => new DateTime,
                 "updated_at" => new DateTime
             ),
-            array(
-                "email_address" => "admin@example.org",
-                "password" => "password",
-                "password_confirmation" => "password",
-                "name" => "Admin User",
-                "locale" => "en",
-                "active" => true,
-                "user_level" => 4,
-                "created_at" => new DateTime,
-                "updated_at" => new DateTime
-            ),
-            array(
-                "email_address" => "active.user@example.org",
-                "password" => "password",
-                "password_confirmation" => "password",
-                "name" => "Active User",
-                "locale" => "en",
-                "active" => true,
-                "user_level" => 1,
-                "created_at" => new DateTime,
-                "updated_at" => new DateTime
-            ),
-            array(
-                "email_address" => "inactive.user@example.org",
-                "password" => "password",
-                "password_confirmation" => "password",
-                "name" => "Inactive User",
-                "locale" => "en",
-                "active" => false,
-                "user_level" => 1,
-                "created_at" => new DateTime,
-                "updated_at" => new DateTime
-            )
         );
 
         DB::table("users")->delete();
