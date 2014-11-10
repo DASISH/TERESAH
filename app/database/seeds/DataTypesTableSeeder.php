1433e50b39ae2719f42a90dc048eedad44840a2a
Olof Olsson <borsna@gmail.com>
2014-11-10 14:12:15 +0100

    adding descriptions to the data types

--- app/database/seeds/DataTypesTableSeeder.php
+++ app/database/seeds/DataTypesTableSeeder.php
@@ -6,16 +6,40 @@ class DataTypesTableSeeder extends Seeder
     {
         $userId = User::first()->id;
         $dataTypes = array(
-            array("label" => "Name", "rdf_mapping" => "http://purl.org/dc/elements/1.1/title", "linkable" => false),
-            array("label" => "Title", "rdf_mapping" => "http://purl.org/dc/elements/1.1/title", "linkable" => false),
-            array("label" => "Description", "rdf_mapping" => "http://purl.org/dc/elements/1.1/description", "linkable" => false),
-            array("label" => "Homepage", "rdf_mapping" => "http://schema.org/url", "linkable" => false),
-            array("label" => "Developer", "rdf_mapping" => "http://purl.org/dc/elements/1.1/creator"),
-            array("label" => "Keyword", "rdf_mapping" => "http://purl.org/dc/elements/1.1/subject"),
-            array("label" => "License", "rdf_mapping" => "http://purl.org/dc/terms/license"),
-            array("label" => "Platform", "rdf_mapping" => "http://schema.org/operatingSystem"),
-            array("label" => "Standard", "rdf_mapping" => "http://purl.org/dc/terms/conformsTo"),
-            array("label" => "Tool Type", "rdf_mapping" => "http://purl.org/dc/elements/1.1/type")
+            array("label" => "Description", 
+                  "rdf_mapping" => "http://purl.org/dc/elements/1.1/description", 
+                  "description" => "General description of the tool",
+                  "linkable" => false
+                 ),
+            array("label" => "Homepage", 
+                  "rdf_mapping" => "http://schema.org/url", 
+                  "description" => "URL to the tool homepage",                
+                  "linkable" => false
+                 ),
+            array("label" => "Developer", 
+                  "rdf_mapping" => "http://purl.org/dc/elements/1.1/creator",
+                  "description" => "Organization or person who developed the tool"
+                 ),
+            array("label" => "Keyword", 
+                  "rdf_mapping" => "http://purl.org/dc/elements/1.1/subject",
+                  "description" => "Free form keywords describing the tool"
+                 ),
+            array("label" => "License", 
+                  "rdf_mapping" => "http://purl.org/dc/terms/license",
+                  "description" => "Type of licence for the tool"
+                 ),
+            array("label" => "Platform", 
+                  "rdf_mapping" => "http://schema.org/operatingSystem",
+                  "description" => "Platform the tool runs on"
+                 ),
+            array("label" => "Standard", 
+                  "rdf_mapping" => "http://purl.org/dc/terms/conformsTo",
+                  "description" => "Suported standard for the tool"
+                 ),
+            array("label" => "Tool Type", 
+                  "rdf_mapping" => "http://purl.org/dc/elements/1.1/type",
+                  "description" => "General type of the tool"
+                 )
         );
 
         DB::table("data_types")->delete();
