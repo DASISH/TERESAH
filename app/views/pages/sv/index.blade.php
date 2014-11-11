5e1600bf15a2ce200137635a3739a05c6bf210bc
Olof Olsson <borsna@gmail.com>
2014-11-11 12:33:34 +0100

    translated start page to swedish

--- app/views/pages/sv/index.blade.php
+++ app/views/pages/sv/index.blade.php
@@ -1,23 +1,119 @@
 @extends("layouts.default")
 
+@section("master-head")
+    <div id="banner">
+        <div class="content align">
+            <div class="row">
+                <div class="small-8 columns small-centered">
+                    <h1>Verktygsresiter för forskare</h1>
+
+                    {{ Form::open(array("action" => "ToolsController@search", "method" => "get", "class" => "row search")) }}
+                        <div class="small-12 columns">
+                            <input type="text" name="query" placeholder="Hitta verktyg, tjänster, metoder och mer..." />
+                        </div>
+                        <!-- /small-12.columns -->
+                    {{ Form::close() }}
+                    <!-- /row.search -->
+                </div>
+                <!-- /small-8.columns.small-centered -->
+            </div>
+            <!-- /row -->
+        </div>
+    </div>
+    <!-- /banner -->
+
+    <div id="partners">
+        <div class="row">
+            <div class="small-12 columns">
+                <ul class="small-block-grid-7">
+                    <li><a href="http://www.cessda.net/" title="CESSDA">{{ image_tag("partners/cessda_logo.png", array("alt" => "CESSDA")) }}</a></li>
+                    <li><a href="http://clarin.eu/" title="Clarin">{{ image_tag("partners/clarin_logo.png", array("alt" => "Clarin")) }}</a></li>
+                    <li><a href="https://www.dariah.eu/" title="DARIAH">{{ image_tag("partners/dariah_logo.png", array("alt" => "DARIAH")) }}</a></li>
+                    <li><a href="http://dasish.eu/" title="DASISH">{{ image_tag("partners/dasish_logo.png", array("alt" => "DASISH")) }}</a></li>
+                    <li><a href="http://www.europeansocialsurvey.org/" title="European Social Survey">{{ image_tag("partners/ess_logo.png", array("alt" => "European Social Survey")) }}</a></li>
+                    <li><a href="http://www.share-project.org/" title="SHARE">{{ image_tag("partners/share_logo.png", array("alt" => "SHARE")) }}</a></li>
+                    <li><a href="http://cordis.europa.eu/fp7/home_en.html" title="Seventh Framework Programme">{{ image_tag("partners/seventh_framework_programme_logo.png", array("alt" => "Seventh Framework Programme")) }}</a></li>
+                </ul>
+                <!-- /small-block-grid-7 -->
+            </div>
+            <!-- /small-12.columns -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /partners -->
+@stop
+
 @section("content")
-    <div class="jumbotron">
-        <h1>Välkommen till TERESAH</h1>
+    <section class="row">
+        <div class="small-10 columns small-centered">
+            <h1 class="icon calendar">Dagens verktyg</h1>
 
-        <p>TERESAH (Tools E-Registry for E-Social science, Arts and Humanities) är ett tvärvetenskapligt verktygsregister riktar sig till forskare inom samhällsvetenskap och humaniora (SSH). Syftet är att ge en auktoritativ notering av mjukvaruverktyg som för närvarande används i dessa områden, och att låta sina användare beskriva metoder och tillämpningar som verktygen möjliggör.</p>
-    </div>
-    <!-- /jumbotron -->
+            <div class="row">
+                <div class="small-7 columns">
+                    <h2><a href="#" title="Ruby on Rails">Ruby on Rails</a></h2>
 
-    <div id="cloud">
-        <p>Laddar molnet...</p>
-    </div>
-    <!-- /cloud -->
+                    <p>Ruby on Rails, often simply referred to as Rails, is an open source web application framework which runs via the Ruby programming language. It is a full-stack framework: it allows creating pages and applications that gather information from the web server, talk to or query the database, and render templates out of the box. As a result, Rails features a routing system that is independent of the web server.</p>
 
-    <!-- Initialize cloud -->
-    <script type="text/javascript">
-        // TODO: Replace the test data
-        $.getJSON( "datacloud.json", function( data ) {
-            $("#cloud").empty().jQCloud(data);
-        });
-    </script>
+                    <p><a href="#" class="more" title="Läs mer">Läs mer om Ruby on Rails</a></p>
+                </div>
+                <!-- /small-7.columns -->
+
+                <div class="small-5 columns">
+                    <article class="tool align row collapse" itemscope itemtype="http://schema.org/SoftwareApplication">
+                        <div class="small-6 columns">
+                            <a href="#" class="symbol large" title="Ruby on Rails"><abbr title="Ruby on Rails">RoR</abbr></a>
+                        </div>
+                        <!-- /small-3.columns -->
+
+                        <div class="small-6 columns">
+                            <h1 itemprop="name"><a href="#" title="Ruby on Rails">Ruby on Rails</a></h1>
+
+                            <p>uppdaterat för 2 dagar sedan</p>
+                        </div>
+                        <!-- /small-9.columns -->
+                    </article>
+                    <!-- /tool.align.row.collapse -->
+                </div>
+                <!-- /small-5.columns -->
+            </div>
+            <!-- /row -->
+        </div>
+        <!-- /small-10.columns.small-centered -->
+    </section>
+    <!-- /row -->
+
+    <section class="row">
+        <div class="small-10 columns small-centered">
+            <div class="row">
+                <div class="small-4 columns">
+                    <h1 class="icon latest"><a href="#" title="Latest Tools">Senast tillagt</a></h1>
+
+                    @foreach ($latestTools as $tool)
+                        @include("tools._tool", compact("tool"))
+                    @endforeach
+                </div>
+                <!-- /small-4.columns -->
+
+                <div class="small-4 columns">
+                    <h1 class="icon most-popular"><a href="#" title="Most Popular">Mest populära</a></h1>
+
+                    @foreach ($mostPopularTools as $tool)
+                        @include("tools._tool", compact("tool"))
+                    @endforeach
+                </div>
+                <!-- /small-4.columns -->
+
+                <div class="small-4 columns">
+                    <h1 class="icon most-used"><a href="#" title="Most Used">Mest använda</a></h1>
+
+                    @foreach ($mostUsedTools as $tool)
+                        @include("tools._tool", compact("tool"))
+                    @endforeach
+                </div>
+                <!-- /small-4.columns -->
+            </div>
+        </div>
+        <!-- /small-10.columns.small-centered -->
+    </section>
+    <!-- /row -->
 @stop
