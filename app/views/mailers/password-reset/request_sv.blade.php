39175ce1daba5ee4406833545be1b15ae2d270f3
jfihn <johan.fihn@gmail.com>
2014-11-14 11:01:48 +0100

    Added missing translations for mailers
    Fixes crash on signup when using Swedish

---
+++ app/views/mailers/password-reset/request_sv.blade.php
@@ -0,0 +1,16 @@
+Hej, {{ $user->firstName }}!
+
+Vi har skickat detta meddelande då du har begärt att ditt lösenord för TERESAH
+ska bli återställt.
+
+Klicka på länken nedan för att skapa ett nytt lösenord.
+
+återställ ditt lösenord
+{{ $url }}
+
+-------------------------------------------------
+
+Om du inte vill ändra ditt lösenord, så kan du ignorera denna epost.
+
+Med vänlig hälsning,
+DASISH/TERESAH-teamet
---
+++ app/views/mailers/signup/welcome_sv.blade.php
@@ -0,0 +1,9 @@
+Välkommen till TERESAH, {{ $user->firstName }}!
+
+Tack för att du har registerat dig för TERESAH (Tools E-Registry for E-Social science, Arts and Humanities). 
+Som en registerad användare kommer du att kunna bidra till innehållet i TERESAH.
+
+Din registrade epostadress och som även används som användarnamn är: {{ $user->email_address }}
+
+Med vänlig hälsning,
+DASISH/TERESAH-teamet
